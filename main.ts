931e12112c915d6660c93ea9487b99f7d5283529
Jesus Esquibel <109695691+jesquibel-CSUB@users.noreply.github.com>
2023-07-19 22:29:43 -0700

    Update main.blocks, main.ts, thermostat.md

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+pins.servoWritePin(AnalogPin.P0, 0)
 basic.forever(function () {
-	
+    basic.showNumber(0)
+    pins.servoWritePin(AnalogPin.P0, pins.map(
+    input.temperature(),
+    -5,
+    50,
+    0,
+    180
+    ))
 })
